48b4a1509fe9196da6ec5439f863dd90f23a6927
Hariharaan S <s.hariharaan.college@gmail.com>
2023-10-30 17:39:47 +0530

    Update on classification method and added login page

--- static/js/segregate.js
+++ static/js/segregate.js
@@ -1,57 +1,73 @@
-
 function getResponse() {
-    Webcam.snap(function (data_uri) {
-        var image = data_uri;
-        // Create FormData and append the image data
-        var formData = new FormData();
-        formData.append("image", image);
-        var count = 0;
+    const canvas = document.getElementById('canvas');
+    const imageData = canvas.toDataURL('image/jpeg');
+    var formData = new FormData();
+    formData.append("image",imageData)
 
-        // Perform the AJAX request inside the callback
-        $.ajax({
-            type: "POST",
-            url: "/classify",
-            data: formData,
-            processData: false,  // Important for FormData
-            contentType: false,  // Important for FormData
-            success: function (response) {
-                console.log(response.prediction);
-                var res = response.prediction
-                if (res != null) {
-                    var temp = res.split('$');
-                    var newEle = document.createElement("tr");
-                    temp.forEach(element => {
-                        var t = element.split("#")[1];
-                        var s = document.createElement("td");
-                        s.textContent = t;
-                        newEle.appendChild(s);
-                    });
+    $.ajax({
+        type: "POST",
+        url: "/classify",
+        data: formData,
+        processData: false,  
+        contentType: false,  
+        success: function (response) {
+            console.log(response.prediction);
+            var res = response.prediction
+            if (res != null) {
+                var temp = res.split('$');
+                var newEle = document.createElement("tr");
+                temp.forEach(element => {
+                    var t = element.split("#")[1];
+                    var s = document.createElement("td");
+                    s.textContent = t;
+                    newEle.appendChild(s);
+                });
 
-                    document.getElementById("result").appendChild(newEle);
-                    document.getElementById("result").style.display = "block";
-                }
-
+                document.getElementById("result").appendChild(newEle);
+                document.getElementById("result").style.display = "block";
             }
-        });
+
+        }
     });
 }
 
-if(window.innerWidth <=500){
-    Webcam.set({
-        width: 200,
-        height: 200,
-        image_format: 'jpeg',
-        jpeg_quality: 100
-    });
+async function startWebcam() {
+    const constraints = {
+        video: {
+            width: {
+              min: 300,
+              ideal: 450,
+              max: 600,
+            },
+            height: {
+              min: 300,
+              ideal: 450,
+              max: 600
+            }},
+        audio: false,
+    };
+
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const webcam = document.getElementById('camera');
+        webcam.srcObject = stream;
+    } catch (error) {
+        console.error('Error accessing the webcam:', error);
+    }
 }
-else{
-    Webcam.set({
-        width: 500,
-        height: 500,
-        image_format: 'jpeg',
-        jpeg_quality: 100
-    });
+
+function captureImage() {
+    const webcam = document.getElementById('camera');
+    const canvas = document.getElementById('canvas');
+    const capturedImage = document.getElementById('capturedImage');
+
+    const context = canvas.getContext('2d');
+    canvas.width = webcam.videoWidth;
+    canvas.height = webcam.videoHeight;
+    context.drawImage(webcam, 0, 0, canvas.width, canvas.height);
+    capturedImage.src = canvas.toDataURL('image/jpeg');
 }
-Webcam.attach("#camera");
+
+window.onload = startWebcam;
 
 setInterval(getResponse, 3000);
